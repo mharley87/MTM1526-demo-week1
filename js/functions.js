7617920bdef04bcbd3f41e777936e4471033f3f4
mharley87 <mike.harley87@yahoo.com>
2012-01-13 12:13:04 -0500

    f

--- js/functions.js
+++ js/functions.js
@@ -2,10 +2,44 @@
 
 alert('this is an alert');
 
-var name = prompt('What Is Your Name');
+var name 
 
+while (!name) { 
+	name = prompt('What is your name?');
+}
 //(trace)
 console.log(name);
 
 document.write('Hello, ' + name );
+
+for (var i = 0; i < 5; i++) {
+	document.write('<br>' + i);
+	document.write('<h1>' + i + '</h1>'); 
+}
+
+function isNameCool () {
+	var name = prompt ('What is your name?');
+	
+	if (name == 'Kait') {
+		document.write('That is an awesome name!');
+	}else{
+		document.write("I dont like your name");
+	}
+}
+
+isNameCool();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
